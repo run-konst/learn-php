60a3d65e12482dcfe298fea5656712c47c94a6fe
Руньковский Константин <raitok@mail.ru>
2021-10-30 23:05:51 +0300

    lesson 5 2b

--- lesson5.php
+++ lesson5.php
@@ -18,33 +18,86 @@
         return $sum *= (100 - $percent) / 100;
     }
 
-    echo implode(', ', $prices) . "\n";
-    echo $discount . "\n";
-    echo get_price($prices, $discount) . "\n";
+    // echo implode(', ', $prices) . "\n";
+    // echo $discount . "\n";
+    // echo get_price($prices, $discount) . "\n";
 
     // 2. Написать 2 функции:
     // a) возвращает правильную форму множественного числа, на вход принимает целое число (кол-во) и массив с 3мя элементами (формы мн. числа существительного). например:
     // имя_функции(5, ['лайк', 'лайка', 'лайков']) вернет "5 лайков" или имя_функции(4, ['комментарий', 'комментария', 'комментариев']) вернет "4 комментария"
-    // b) возвращает сумму прописью (без копеек), на вход принимает целое число от 1 до 999 999
-
-    function get_word_form(int $num, array $words = ['лайк', 'лайка', 'лайков']) {
+    
+    function get_word_form(int $num, array $words = ['лайк', 'лайка', 'лайков'], bool $echo_num = true) {
         if ( count($words) !== 3 ) return '';
+        $word = '';
         $last_num = substr($num, -2);
         if ($last_num > 10 && $last_num < 20) {
-            return "$num $words[2]";
-        }
-        $last_num = substr($last_num, -1);
-        if ($last_num == 1) {
-            return "$num $words[0]";
-        } elseif ($last_num >= 1 && $last_num <= 4) {
-            return "$num $words[1]";
+            $word = "$words[2]";
         } else {
-            return "$num $words[2]";
+            $last_num = substr($last_num, -1);
+            if ($last_num == 1) {
+                $word = "$words[0]";
+            } elseif ($last_num >= 1 && $last_num <= 4) {
+                $word = "$words[1]";
+            } else {
+                $word = "$words[2]";
+            }
         }
-        return "$num $word";
+        if ($echo_num) {
+            return "$num $word";
+        }
+        return $word;
     }
 
-    echo get_word_form(rand(0, 500));
+    echo get_word_form(rand(0, 500)) . "\n";
+
+    // b) возвращает сумму прописью (без копеек), на вход принимает целое число от 1 до 999 999
+
+    $hundreds = ['сто', 'двести', 'триста', 'четыреста', 'пятьсот', 'шестьсот', 'семьсот', 'восемьсот', 'девятьсот'];
+    $dozens = ['десять', 'двадцать', 'тридцать', 'сорок', 'пятьдесят', 'шестьдесят', 'семьдесят', 'восемьдесят', 'девяносто'];
+    $units_thousand = ['одна', 'две', 'три', 'четыре', 'пять', 'шесть', 'семь', 'восемь', 'девять'];
+    $units_rub = ['один', 'два', 'три', 'четыре', 'пять', 'шесть', 'семь', 'восемь', 'девять'];
+
+    function match_num(array $word_arr, string $num, int $pos = 0) {
+        for ($i = 0; $i < count($word_arr); $i++) {
+            if ($num[$pos] == 0) return '';
+            if ($num[$pos] == $i + 1) {
+                return $word_arr[$i] . ' ';
+            }
+        }
+    }
+
+    function sum_to_words(int $num) {
+        global $hundreds;
+        global $dozens;
+        global $units_thousand;
+        global $units_rub;
+        $thousands = (string) floor($num / 1000);
+        $units = substr($num, -3);
+        $result = '';
+        if (!empty($thousands)) {
+            if (strlen($thousands) === 3) {
+                $result .= match_num($hundreds, $thousands) . match_num($dozens, $thousands, 1) . match_num($units_thousand, $thousands, 2);
+            } elseif (strlen($thousands) === 2) {
+                $result .= match_num($dozens, $thousands) . match_num($units_thousand, $thousands, 1);
+            } else {
+                $result .= match_num($units_thousand, $thousands);
+            }
+            $result .= get_word_form($thousands, ['тысяча', 'тысячи', 'тысяч'], false) . ' ';
+        }
+        if (!empty($units)) {
+            if (strlen($units) === 3) {
+                $result .= match_num($hundreds, $units) . match_num($dozens, $units, 1) . match_num($units_rub, $units, 2);
+            } elseif (strlen($units) === 2) {
+                $result .= match_num($dozens, $units) . match_num($units_rub, $thousands, 1);
+            } else {
+                $result .= match_num($units_rub, $units);
+            }
+            $result .= get_word_form($units, ['рубль', 'рубля', 'рублей'], false);
+        }
+        return $result;
+    }
+
+    echo sum_to_words(111050);
 
     // 3. Написать функцию сортировки массива из строк по длине строк. За основу можно взять текст из домашнего задания к прошлому уроку или другой рыбу-текст (на латинице) и разбить его на слова. На вход должна принимать массив строк, возвращать его же, но отсортированный по возрастанию длины строки.
 
@@ -64,4 +117,4 @@
     }
 
     usort($text_arr, fn($word1, $word2) => strlen($word1) < strlen($word2) ? -1 : 1);
-    var_dump($text_arr);
+    //var_dump($text_arr);
